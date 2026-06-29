24359170b8ebe9d0ef5a064e0203840e645ef38e
dgpetrie <dpetrie@sipez.com>
2026-06-29 18:50:33 -0400

    fixed text/code line wrapper.  Was repeating last character on first wrapped line

--- examples/trunc_lines.sh
+++ examples/trunc_lines.sh
@@ -9,18 +9,15 @@ awk 'BEGIN {max = 65}
         match($0, /^ */);
         #print(RLENGTH " spaces")
         pref = RLENGTH + 2
-        trunk = max; 
-        start = 0; 
+        start = 1; 
         prefix = 0; 
-        while(start < len) 
+        while(start <= len) 
         {
           # print(start, trunk);
           line_len = max - prefix
-          if(line_len + start > len) line_len = len - start + 1
           printf("%*s%s\n", prefix, "", substr($0, start, line_len))
           #print(prefix substr($0, start, line_len));
-          start = trunk + 1;
-          trunk = start - 1 + max - pref;
+          start = start + line_len;
           prefix = pref
         }
       }
